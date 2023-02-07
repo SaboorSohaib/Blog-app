1e419aaa6fdd776a72ae5da2cf2d4099c24abe09
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-02-07 20:22:19 +0430

    Add posts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_07_154933) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_07_155142) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_07_154933) do
   end
 
   create_table "likes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "Title"
+    t.text "Text"
+    t.integer "CommentsCounter"
+    t.integer "LikesCounter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
