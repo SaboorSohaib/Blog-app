4aa2771a30faecc2a83dd5540d85ea08f23f3aeb
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-02-07 20:32:00 +0430

    rename users_id to AuthorId

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_07_155750) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_07_160007) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,8 +18,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_07_155750) do
     t.text "Text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "users_id", null: false
-    t.index ["users_id"], name: "index_comments_on_users_id"
+    t.bigint "AuthorId", null: false
+    t.index ["AuthorId"], name: "index_comments_on_AuthorId"
   end
 
   create_table "likes", force: :cascade do |t|
@@ -45,5 +45,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_07_155750) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "comments", "users", column: "users_id"
+  add_foreign_key "comments", "users", column: "AuthorId"
 end
