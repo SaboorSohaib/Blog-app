94e9194d6241cb98d8777b1741ce27a9b190cff8
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-02-07 20:39:29 +0430

    Add foreign key posts_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_07_160601) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_07_160842) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,6 +27,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_07_160601) do
   create_table "likes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "posts_id", null: false
+    t.index ["posts_id"], name: "index_likes_on_posts_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -49,4 +51,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_07_160601) do
 
   add_foreign_key "comments", "posts", column: "PostId"
   add_foreign_key "comments", "users", column: "AuthorId"
+  add_foreign_key "likes", "posts", column: "posts_id"
 end
