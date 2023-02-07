683937906804ca64e0826ca957ee19df17be40ce
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-02-07 20:25:21 +0430

    Add users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_07_155142) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_07_155442) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_07_155142) do
     t.text "Text"
     t.integer "CommentsCounter"
     t.integer "LikesCounter"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "Name"
+    t.string "Photo"
+    t.text "Bio"
+    t.integer "PostsCounter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
