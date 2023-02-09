a82b800a4d8f4270210828cbce75379791e63854
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-02-09 10:37:41 +0430

    Add most rencent post method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
-    has_many :comments, foreign_key: 'users_id'
-    has_many :likes, foreign_key: 'users_id'
-    has_many :posts, foreign_key: 'users_id'
+  has_many :comments, foreign_key: 'users_id'
+  has_many :likes, foreign_key: 'users_id'
+  has_many :posts, foreign_key: 'users_id'
+  after_save :most_recent_post
+
+  def most_recent_post
+    posts.order(created_at: :desc).includes(:user).limit(3)
+  end
 end
