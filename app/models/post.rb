f085f908318045055ca4261c0219e433c11c3115
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-02-09 10:41:50 +0430

    Add most recent comment method

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ApplicationRecord
     has_many :comments, foreign_key: 'posts_id'
     has_many :likes, foreign_key: 'posts_id'
     belongs_to :user, foreign_key: 'users_id'
+
+    def most_recent_comment
+        comments.where(posts_id: id).order(created_at: :desc).limit(5)
+    end
 end
