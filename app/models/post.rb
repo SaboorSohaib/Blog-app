9b6041ea7ff2dcc881e2574495bc8e047a17af3e
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-02-09 10:46:59 +0430

    Add after-save callback

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   has_many :comments, foreign_key: 'posts_id'
   has_many :likes, foreign_key: 'posts_id'
   belongs_to :user, foreign_key: 'users_id'
+  after_save :update_posts_counter
 
   def most_recent_comment
     comments.where(posts_id: id).order(created_at: :desc).limit(5)
