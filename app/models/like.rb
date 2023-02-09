e84a5c2a93b4ce3f39edaf52512d20d6b2a98b68
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-02-09 10:32:35 +0430

    Add after_save callback

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,8 +1,9 @@
 class Like < ApplicationRecord
-    belongs_to :user, foreign_key: 'users_id'
-    belongs_to :post, foreign_key: 'posts_id'
+  belongs_to :user, foreign_key: 'users_id'
+  belongs_to :post, foreign_key: 'posts_id'
+  after_save :update_likes_counter
 
-    def update_likes_counter
-        post.increment!(likes_counter)
-    end
+  def update_likes_counter
+    post.increment!(likes_counter)
+  end
 end
