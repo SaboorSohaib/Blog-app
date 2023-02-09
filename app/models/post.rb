6665b3800ba2365813c244a83ab135cd0afff4f5
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-02-09 10:43:54 +0430

    Add update posts counter

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,13 @@
 class Post < ApplicationRecord
-    has_many :comments, foreign_key: 'posts_id'
-    has_many :likes, foreign_key: 'posts_id'
-    belongs_to :user, foreign_key: 'users_id'
+  has_many :comments, foreign_key: 'posts_id'
+  has_many :likes, foreign_key: 'posts_id'
+  belongs_to :user, foreign_key: 'users_id'
 
-    def most_recent_comment
-        comments.where(posts_id: id).order(created_at: :desc).limit(5)
-    end
+  def most_recent_comment
+    comments.where(posts_id: id).order(created_at: :desc).limit(5)
+  end
+
+  def update_posts_counter
+    user.increment!(posts_counter)
+  end
 end
