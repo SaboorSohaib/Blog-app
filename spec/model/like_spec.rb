f14da707237a9279caebad5cc3ca1c3a0b2b2861
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-02-10 15:22:00 +0430

    Add like author_id test to be integr

--- spec/model/like_spec.rb
+++ spec/model/like_spec.rb
@@ -9,5 +9,10 @@ RSpec.describe Like, type: :model do
         subject.author_id = 'aaa'
         expect(subject).to_not be_valid
     end
+
+    it 'The author_id should be integer' do
+        id = subject.author_id = 2
+        expect(id).to be == 2
+    end
   end
 end
