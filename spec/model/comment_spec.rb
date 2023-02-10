f11984732595732fd99b57014e8ab0f0af27a6fd
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-02-10 15:09:09 +0430

    Add test for author_id to be number

--- spec/model/comment_spec.rb
+++ spec/model/comment_spec.rb
@@ -1,1 +1,18 @@
 require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+  describe 'Comment Model Test' do
+    subject { Comment.new(text: 'Good Job', author_id: 2, posts_id: 2) }
+    before { subject.save }
+
+    it 'The comment should not be blank' do
+      subject.text.nil?
+      expect(subject).to_not be_valid
+    end
+
+    it 'The author_id should be number' do
+        subject.author_id = 'aaa'
+        expect(subject).to_not be_valid
+    end
+  end
+end
