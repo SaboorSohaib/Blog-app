d66034410577ba1fa4d0b8af7c557d835e915a2e
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-02-10 15:21:13 +0430

    Add like author_id test

--- spec/model/like_spec.rb
+++ spec/model/like_spec.rb
@@ -1,1 +1,13 @@
 require 'rails_helper'
+
+RSpec.describe Like, type: :model do
+  describe 'Like Model Test' do
+    subject { Like.new(author_id: 5, posts_id: 5) }
+    before { subject.save }
+
+    it 'The author_id should be number' do
+        subject.author_id = 'aaa'
+        expect(subject).to_not be_valid
+    end
+  end
+end
