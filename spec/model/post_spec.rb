b3cd76ebcd4f72a4bd3816127606d02256f0e75f
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-02-10 14:51:00 +0430

    Add title test

--- spec/model/post_spec.rb
+++ spec/model/post_spec.rb
@@ -1,1 +1,11 @@
 require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  subject { Post.new(title: 'Javascript', text: 'porgramming language', comments_counter: 10, likes_counter: 10) }
+  before { subject.save }
+
+  it 'The title should be present' do
+    subject.title.nil?
+    expect(subject).to_not be_valid
+  end
+end
