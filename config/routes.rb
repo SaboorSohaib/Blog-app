fbf142806ec15970cc85247155f4544812242c3e
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-02-13 16:01:02 +0430

    remove comment form routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  # get 'users/index'
-  # get 'users/show'
-  # get 'users/posts'
-  # get 'posts/index'
-  # get 'posts/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
