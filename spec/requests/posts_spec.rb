90a6aebcb9df9baf463facb64d7d2e0e12f54880
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-02-14 15:40:53 +0430

    Add render template rspec for posts show action

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -17,6 +17,9 @@ RSpec.describe 'PostsController', type: :request do
     it 'is a success' do
       expect(response).to have_http_status(:ok)
     end
+    it "renders 'show' template" do
+      expect(response).to render_template(:show)
+    end
     
   end
 end
