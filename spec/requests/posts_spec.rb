05e329fb10cb834ad071e837923cc865db84015f
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-02-14 15:39:35 +0430

    Add http status rspec for posts show action

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -11,6 +11,9 @@ RSpec.describe 'PostsController', type: :request do
 
   describe 'GET posts_controller#show' do
     before(:example) { get '/users/:user_id/posts/:post_id' } # get(:index)
+    it 'is a success' do
+      expect(response).to have_http_status(:ok)
+    end
     
   end
 end
