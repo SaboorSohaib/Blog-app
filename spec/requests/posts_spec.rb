7feaab20b63814628a7b0f414f67040b78be2824
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-02-14 15:38:50 +0430

    Add http status rspec for posts index action

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -3,6 +3,9 @@ require 'rails_helper'
 RSpec.describe 'PostsController', type: :request do
   describe 'GET posts_controller#index' do
     before(:example) { get '/users/:user_id/posts' } # get(:index)
+    it 'is a success' do
+      expect(response).to have_http_status(:ok)
+    end
     
   end
 
