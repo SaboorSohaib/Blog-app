1b76a36b2439f74c2a8ae7e8c143149663075299
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-02-16 13:58:41 +0430

    Add users show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:id])
+    @posts = Post.where(:author_id)
   end
 
   def show
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,8 @@ class UsersController < ApplicationController
   end
 
   def show
+    # @user = User.find(params[:id])
     @user = User.find(params[:id])
+    @recent_posts = @user.most_recent_post
   end
 end
