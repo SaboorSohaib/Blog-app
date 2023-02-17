4ce64d9744d233826fee0d859626957898ac121f
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-02-17 22:08:11 +0430

    Add likes controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,11 @@
 class LikesController < ApplicationController
+    def create
+        user = current_user
+        @like = Like(
+            posts_id: params[:posts_id]
+            author_id: user.id
+        )
+        return unless @like.save
+        redirect_to user_posts_path
+    end
 end
