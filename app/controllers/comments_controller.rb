24e933471b2c188302d3fb6d11c2351bbaf2d437
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-02-17 22:05:31 +0430

    Add comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,12 @@
 class CommentsController < ApplicationController
+    def create
+        user = current_user
+        @comment = Comment.new(
+            text: params[:text]
+            posts_id: params[:posts_id]
+            author_id: user.id
+        )
+        return unless @comment.save
+        redirect_to request.original_url
+    end
 end
