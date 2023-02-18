1e8350d6162ffb75d08064843c75da5dd6d4c29b
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-02-18 09:39:02 +0430

    fix association

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: 'User'
+  belongs_to :user, class_name: 'User'
   has_many :comments
   has_many :likes
   after_save :update_posts_counter
