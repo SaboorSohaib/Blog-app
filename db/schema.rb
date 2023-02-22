590ffe89da756d3dacf203d34c4622b4fd0996ea
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-02-22 14:28:41 +0430

    Add data to database

--- db/schema.rb
+++ db/schema.rb
@@ -16,17 +16,21 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_22_092349) do
 
   create_table "comments", force: :cascade do |t|
     t.bigint "author_id", null: false
+    t.bigint "post_id", null: false
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_comments_on_author_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
   create_table "likes", force: :cascade do |t|
     t.bigint "author_id", null: false
+    t.bigint "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_likes_on_author_id"
+    t.index ["post_id"], name: "index_likes_on_post_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -49,7 +53,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_22_092349) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users", column: "author_id"
+  add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users", column: "author_id"
   add_foreign_key "posts", "users", column: "author_id"
 end
