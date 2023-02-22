b3e1b7efa18523bdbffe4601efca7121f5e9d569
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-02-22 15:52:31 +0430

    fix linters error

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,7 @@ class Post < ApplicationRecord
   validates :comments_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
   validates :likes_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
   after_save :update_posts_counter
-  
+
   def most_recent_comment
     comments.order(created_at: :desc).limit(5)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,6 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
 
-
   def most_recent_post
     posts.order(created_at: :desc).limit(3)
   end
