9db33c3d725a6fbe69727a94f837c38d495a6153
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-02-22 15:05:38 +0430

    fix some issues

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,7 +7,7 @@ class Post < ApplicationRecord
   validates :likes_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
   after_save :update_posts_counter
   
-  def recent_five
+  def most_recent_comment
     comments.order(created_at: :desc).limit(5)
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   validates :posts_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
 
 
-  def recent_three
+  def most_recent_post
     posts.order(created_at: :desc).limit(3)
   end
 end
