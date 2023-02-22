3a2bba8f04d7ec89d23821db2296b180030dc6ab
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-02-22 14:41:40 +0430

    Add post validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,12 @@
 class Post < ApplicationRecord
-  after_save :update_posts_counter
   belongs_to :author, class_name: 'User'
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
-
+  validates :title, presence: true
+  validates :comments_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
+  after_save :update_posts_counter
+  
   def recent_five
     comments.order(created_at: :desc).limit(5)
   end
