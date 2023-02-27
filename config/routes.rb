0bc892d9bb9eb8f5cd394a4095491328b4ab14f9
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-02-27 15:17:51 +0430

    Add some changes

--- config/routes.rb
+++ config/routes.rb
@@ -4,17 +4,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root to: 'users#index'
-  get '/posts/new', to: 'posts#new', as: 'create_post'
-  post '/posts/new', to: 'posts#create'
-  post '/users/:user_id/posts/:id/', to: 'posts#create_comment'
-  post '/users/:user_id/posts/:id/likes', to:'posts#create_like', as: 'like'
-  post '/comments', to: 'comments#new'
-  post '/comments/:post_id', to: 'comment#create'
-  post '/likes/:post_id', to: 'likes#create'
-  
   resources :users, only:[:index, :show] do
-    resources :posts, only:[:index, :show, :new, :create] 
-      # resources :comments, only:[:new, :create]
-      # resources :likes, only: [:new, :create]
+    resources :posts, only:[:index, :show, :new, :create] do
+      resources :comments, only:[:new, :create]
+      resources :likes, only: [:new, :create]
+    end
   end
 end
