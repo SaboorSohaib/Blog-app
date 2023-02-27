24378e98dfd6ad9e11e2cdcde4ac52123913379b
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-02-27 17:48:13 +0430

    Add users show page tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @posts = Post.find(params[:id])
+    # @posts = Post.find(params[:id])
   end
 end
--- spec/features/show.html.erb_spec.rb
+++ spec/features/show.html.erb_spec.rb
@@ -1,37 +1,50 @@
 require 'rails_helper'
 
-RSpec.describe 'show.html.erb_spec.rb', type: :feature do
-  describe 'shwo page' do
+RSpec.describe 'users/show.html.rb', type: :feature do
+  describe 'show page' do
     before(:each) do
       @user1 = User.create!(name: 'Ali', photo: 'https://unsplash.com/photos/WNoLnJo7tS8', posts_counter: 4,
                             bio: 'developer')
-      @user2 = User.create!(name: 'Abid', photo: 'https://unsplash.com/photos/WNoLnJo7tS8', posts_counter: 5,
+      @user2 = User.create!(name: 'Abid', photo: 'https://unsplash.com/photos/WNoLnJo7tS8', posts_counter: 4,
                             bio: 'Engineer')
 
       visit root_path
 
-      @post = Post.create!(title: 'Football', text: 'I like football', comments_counter: 4,
-                            likes_counter: 4, user_id: @user1.id)
-      @post = Post.create!(title: 'Criket', text: 'He likes criket', comments_counter: 5,
-                            likes_counter: 5, user_id: @user2.id)
+      @post1 = Post.create!(title: 'Football', text: 'I like football', comments_counter: 4,
+                           likes_counter: 4, author_id: @user1.id)
+      @post2 = Post.create!(title: 'Criket', text: 'He likes criket', comments_counter: 4,
+                           likes_counter: 4, author_id: @user1.id)
+      @post3 = Post.create!(title: 'Tennis', text: 'He likes tennis', comments_counter: 4,
+                            likes_counter: 4, author_id: @user1.id)
 
       visit user_path(@user1.id)
     end
+
     it 'dispalys the user name' do
-        visit users_path
-        expect(page).to have_content(@user1.name)
-        expect(page).to have_content(@user2.name)
-      end
-      it 'dispalys user image' do
-        visit users_path
-        expect(page).to have_css("img[src='https://unsplash.com/photos/WNoLnJo7tS8']")
-        expect(page).to have_css("img[src='https://unsplash.com/photos/WNoLnJo7tS8']")
-      end
-      it 'display user number of post' do
-        visit users_path
-        expect(page).to have_content(@user1.posts_counter)
-        expect(page).to have_content(@user2.posts_counter)
-      end
-  
+      expect(page).to have_content(@user1.name)
+    end
+
+    it 'dispalys user image' do
+      expect(page).to have_css("img[src='https://unsplash.com/photos/WNoLnJo7tS8']")
+    end
+
+    it 'display user number of post' do
+      expect(page).to have_content(@user1.posts_counter)
+    end
+
+    it 'dsiplay 3 most recent post' do 
+      expect(page).to have_content(@post3.title)
+      expect(page).to have_content(@post2.title)
+      expect(page).to have_content(@post1.title)
+    end
+
+    it 'display button to see all posts' do 
+        expect(page).to have_link('See All Posts')
+    end
+
+    it 'click see all posts and redirects to user posts index page' do
+        click_link 'See All Posts'
+        expect(page).to have_current_path user_posts_path(@user1)
+    end
   end
 end
