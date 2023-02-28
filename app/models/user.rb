0038a4bf3b540519ec8f89478cc6a65939086724
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-02-28 18:38:10 +0430

    Add required changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
   has_many :posts, foreign_key: 'author_id', dependent: :destroy
   has_many :comments, foreign_key: 'author_id', dependent: :destroy
   has_many :likes, foreign_key: 'author_id', dependent: :destroy
-  # validates :name, presence: true
-  # validates :posts_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
-
+ 
   def most_recent_post
     posts.order(created_at: :desc).limit(3)
   end
