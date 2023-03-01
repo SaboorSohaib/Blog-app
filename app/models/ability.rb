009c6ca428d48f1269a3fcac78f522fc3fca4636
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-03-01 15:24:03 +0430

    fix linter errors

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,9 +1,9 @@
 class Ability
   include CanCan::Ability
   def initialize(user)
-  can :manage, :all if user.role == 'admin'
+    can :manage, :all if user.role == 'admin'
 
-  can :destroy, Post, author_id: user.id
-  can :destroy, Comment, author_id: user.id
+    can :destroy, Post, author_id: user.id
+    can :destroy, Comment, author_id: user.id
   end
 end
