49cdd2e01a99c1b32a9fd7ac88788c3b476e70f1
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-03-02 14:34:22 +0430

    Add commnet api

---
+++ app/controllers/api/comments_controller.rb
@@ -0,0 +1,26 @@
+module Api
+  module V1
+    class CommentsController < ApiController
+      def index
+        @post = Post.find(params[:post_id])
+        render json: @post.comments
+      end
+
+      def create
+        @comment = Comment.new(comment_params)
+
+        if @comment.save
+          render json: @comment, status: :created
+        else
+          render json: @comment.errors, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def comment_params
+        params.require(:comment).permit(:author_id, :post_id, :text)
+      end
+    end
+  end
+end
