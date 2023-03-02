47b0b9b533b6a6820feafa68e60208c2a10a7628
AbdoNJ <mr.nejjari.abdelhafid@gmail.com>
2023-03-02 15:26:25 +0100

    Add required changes

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,10 +1,10 @@
-module Api
-  module V1
-    class PostsController < ApiController
-      def index
-        @posts = Post.where(author_id: params[:user_id])
-        render json: @posts
-      end
-    end
+class Api::PostsController < ApiController
+  skip_before_action :authenticate_user!, only: %i[index show]
+
+  def index
+    @posts = User.find(params[:user_id]).posts
+    render json: @posts
   end
+
+  def show; end
 end
