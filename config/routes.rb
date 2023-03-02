2fe1afdec0096832bae55882c7e267b1527aa718
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-03-02 16:02:59 +0430

    Add api files to another folder

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only:[:index, :show] do
-        resources :posts, only:[:index] do
-          resources :comments only:[:index, :create]
+        resources :posts, only:[:index, :show] do
+          resources :comments, only:[:index, :create]
         end
       end
     end
