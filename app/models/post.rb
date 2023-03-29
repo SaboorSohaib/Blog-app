88555b45917e9824bc65abdde8d734647da3b74b
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-03-29 15:59:13 +0430

    FIX LINTER ERRORS

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,15 +1,15 @@
 class Post < ApplicationRecord
-    has_many :comments
-    has_many :likes
-    belongs_to :author, class_name: 'User'
+  has_many :comments
+  has_many :likes
+  belongs_to :author, class_name: 'User'
 
-    after_save :update_posts_counter
+  after_save :update_posts_counter
 
-    def update_posts_counter
-        author.update(posts_counter: author.posts.all.length)
-    end
+  def update_posts_counter
+    author.update(posts_counter: author.posts.all.length)
+  end
 
-    def most_recent_comments
-        Comment.limit(5).order(created_at: :desc)
-    end
+  def most_recent_comments
+    Comment.limit(5).order(created_at: :desc)
+  end
 end
