8a63a3c2662cf67d4c657bb8daf676a8bc6be3eb
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-03-29 15:53:20 +0430

    ADD DATE TO DATABASE

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,14 @@ class Post < ApplicationRecord
     has_many :comments
     has_many :likes
     belongs_to :author, class_name: 'User'
+
+    after_save :update_posts_counter
+
+    def update_posts_counter
+        author.update(posts_counter: author.posts.all.length)
+    end
+
+    def most_recent_comments
+        Comment.limit(5).order(created_at: :desc)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
-    has_many :posts
-    has_many :comments
-    has_many :likes
+    has_many :posts, foreign_key: 'author_id'
+    has_many :comments, foreign_key: 'author_id'
+    has_many :likes, foreign_key: 'author_id'
+
+    def most_recent_posts
+        Post.limit(3).order(created_at: :desc)
+    end
 end
