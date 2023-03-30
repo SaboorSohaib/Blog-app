fd4a2c9d2837bbac4fbd1991f70cc69ba510044b
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-03-30 15:37:45 +0430

    CORRECT POST TEST

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,7 @@ class Post < ApplicationRecord
   after_save :update_posts_counter
 
   validates :title, presence: true, length: { maximum: 250 }
-  validates :commentss_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
+  validates :comments_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
   validates :likes_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
 
   def update_posts_counter
