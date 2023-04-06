82c5f6af6b4f523e2de35ba3e70b27c36c2747a0
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-04-06 14:15:03 +0430

    ADD ROUTES FOR POST, COMMENT and LIKE

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "users#index"
+  get 'posts/new', to: 'posts#new', as: create_post
+  post 'posts/create', to: 'posts#create'
+  get 'users/:user_id/posts/:id', to: 'posts#show', as: 'post'
+  post 'users/:user_id/posts/:id', to: 'posts#create_comment'
+  post 'users/:user_id/posts/:id/likes', to: 'posts#create_like', as: 'like'
+  
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
   end
