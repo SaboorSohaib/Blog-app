67add652e0ec23ccc82367316459a19029249ed1
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-04-06 13:57:05 +0430

    ADD CREATE METHOD FOR POST AND COMMENT

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,15 +6,38 @@ class PostsController < ApplicationController
   def show
     @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
+    @comment = Comment.new
+    @like = Like.new
   end
 
-  def new; end
+  def create_comment
+    @comment = Comment.new(comment_prarams)
+    @comment.author = current_user
+    @comment.post = Post.find(params[:id])
+    @comment.save
+    redirect_to user_post_path
+  end
 
-  def create; end
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.author = current_user
+    @post.comments_counter = 0
+    @post.likes_counter = 0
+    return unless @post.save
+    redirect_to user_post_path
+  end
 
   private
 
   def post_params
     params.require(:post).permit(:title, :text)
+  end
+
+  def comment_prarams
+    params.require(:comment).permit(:text)
   end
 end
