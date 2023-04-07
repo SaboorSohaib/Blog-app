38279d811570d3c5544803462a848a9978aabcc9
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-04-07 23:54:48 +0430

    REMOVE COMMENTED CODE FROM ROUTES FILE

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "users#index"
-  # get 'posts/new', to: 'posts#new', as: 'create_post'
-  # post 'posts/create', to: 'posts#create'
-  # get 'users/:user_id/posts/:id', to: 'posts#show', as: 'post'
-  # post 'users/:user_id/posts/:id', to: 'comments#create'
-  # post 'users/:user_id/posts/:id/likes', to: 'likes#create_like', as: 'like'
-
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
       resources :comments, only: [:new, :create] 
