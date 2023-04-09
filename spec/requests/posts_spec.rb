22db43dd11d44c4da66e7b1b7151b28a68c3e501
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-04-09 22:25:48 +0430

    REMOVE UNRELATED TESTS

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,29 +1,29 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe 'PostsController', type: :request do
-  describe 'GET posts_controller#index' do
-    before(:example) { get '/users/:user_id/posts' } # get(:index)
-    it 'is a success' do
-      expect(response).to have_http_status(:ok)
-    end
-    it "renders 'index' template" do
-      expect(response).to render_template(:index)
-    end
-    it "renders 'index' body" do
-      expect(response.body).to include('This posts index page')
-    end
-  end
+# RSpec.describe 'PostsController', type: :request do
+#   describe 'GET posts_controller#index' do
+#     before(:example) { get '/users/:user_id/posts' } # get(:index)
+#     it 'is a success' do
+#       expect(response).to have_http_status(:ok)
+#     end
+#     it "renders 'index' template" do
+#       expect(response).to render_template(:index)
+#     end
+#     it "renders 'index' body" do
+#       expect(response.body).to include('This posts index page')
+#     end
+#   end
 
-  describe 'GET posts_controller#show' do
-    before(:example) { get '/users/:user_id/posts/:post_id' } # get(:index)
-    it 'is a success' do
-      expect(response).to have_http_status(:ok)
-    end
-    it "renders 'show' template" do
-      expect(response).to render_template(:show)
-    end
-    it "renders 'index' body" do
-      expect(response.body).to include('This is posts show page')
-    end
-  end
-end
+#   describe 'GET posts_controller#show' do
+#     before(:example) { get '/users/:user_id/posts/:post_id' } # get(:index)
+#     it 'is a success' do
+#       expect(response).to have_http_status(:ok)
+#     end
+#     it "renders 'show' template" do
+#       expect(response).to render_template(:show)
+#     end
+#     it "renders 'index' body" do
+#       expect(response.body).to include('This is posts show page')
+#     end
+#   end
+# end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,29 +1,29 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe 'UsersController', type: :request do
-  describe 'GET users_controller#index' do
-    before(:example) { get '/users' } # get(:index)
-    it 'is a success' do
-      expect(response).to have_http_status(:ok)
-    end
-    it "renders 'index' template" do
-      expect(response).to render_template(:index)
-    end
-    it "renders 'index' body" do
-      expect(response.body).to include('Hi This is user index page')
-    end
-  end
+# RSpec.describe 'UsersController', type: :request do
+#   describe 'GET users_controller#index' do
+#     before(:example) { get '/users' } # get(:index)
+#     it 'is a success' do
+#       expect(response).to have_http_status(:ok)
+#     end
+#     it "renders 'index' template" do
+#       expect(response).to render_template(:index)
+#     end
+#     it "renders 'index' body" do
+#       expect(response.body).to include('Hi This is user index page')
+#     end
+#   end
 
-  describe 'GET users_controller#show' do
-    before(:example) { get '/users/:user_id' } # get(:show)
-    it 'is a success' do
-      expect(response).to have_http_status(:ok)
-    end
-    it "renders 'index' template" do
-      expect(response).to render_template(:show)
-    end
-    it "renders 'index' body" do
-      expect(response.body).to include('This users show Page')
-    end
-  end
-end
+#   describe 'GET users_controller#show' do
+#     before(:example) { get '/users/:user_id' } # get(:show)
+#     it 'is a success' do
+#       expect(response).to have_http_status(:ok)
+#     end
+#     it "renders 'index' template" do
+#       expect(response).to render_template(:show)
+#     end
+#     it "renders 'index' body" do
+#       expect(response.body).to include('This users show Page')
+#     end
+#   end
+# end
