f10fb715bbbf8045a5f99f988ceeecc114712adc
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-04-11 23:11:51 +0430

    ADD NAME COLUMN FOR USER SIGN UP

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,13 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
+
   has_many :posts, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
 
   validates :name, presence: true
-  validates :posts_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
 
   def most_recent_posts
     Post.limit(3).order(created_at: :desc)
