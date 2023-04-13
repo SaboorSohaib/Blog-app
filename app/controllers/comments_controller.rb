0923d8c3f0434f738e9d188e74c7bbef83b39733
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-04-13 22:50:28 +0430

    ADD DESTROY METHOD FOR COMMENT

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,6 +12,12 @@ class CommentsController < ApplicationController
     redirect_to user_post_path(current_user.id, params[:post_id])
   end
 
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to user_post_path(current_user.id, params[:post_id])
+  end
+
   private
 
   def comment_params
