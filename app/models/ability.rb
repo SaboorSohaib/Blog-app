f8b502a3d5bbcdd33c2692c748c9ec8da3c11dc8
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-04-13 22:34:58 +0430

    ADD ABILITY TO USER

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,6 +4,9 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
- 
+    can :manage, :all if user.admin?
+
+    can :manage, Post, author_id: user.id
+    can :manage, Comment, author_id: user.id
   end
 end
